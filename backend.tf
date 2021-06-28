13b3070f9b1ac341ab33e0eac8569c0d97466ed8
Khushal Parmar <khushal_parmar@yahoo.co.in>
2021-06-28 12:13:18 +0000

    new

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   # The modules used in this example require Terraform 0.12, additionally we depend on a bug fixed in version 0.12.7.
   required_version = ">= 0.12.7"
   backend "gcs" {
-    bucket  = "github"
+    bucket  = "githubspanner33"
     prefix  = "terraform/state"
     credentials = "spanner33-33b118fd26d6.json"
   }
